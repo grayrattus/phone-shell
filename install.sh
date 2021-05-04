643403ee0307ce5523dd2e4920c36fb23ef82070
grayrattus <arturdziedziczak0@gmail.com>
2021-05-04 17:59:14 +0200

    Add desktop and Termux phone installation

--- install.sh
+++ install.sh
@@ -1,7 +1,15 @@
 mkdir ~/.config/nvim
 cp dnvim.vim ~/.config/nvim/dnvim.vim
 zsh generate-emojis-file.sh
+chmod +x emoji.sh
+phoneTermuxDirectory="/data/data/com.termux/files/usr/bin"
+
+if [ -d "$phoneTermuxDirectory" ]; then
+	cp emoji.sh $phoneTermuxDirectory/emoji
+else 
+	cp emoji.sh /usr/local/bin/emoji
+fi
 cp .profile ~/.dnvim-profile
-if ! grep -q dnvim-profile "~/.zshrc"; then
+if ! grep -q dnvim-profile ~/.zshrc; then
 	echo "source ~/.dnvim-profile" >> ~/.zshrc
 fi
